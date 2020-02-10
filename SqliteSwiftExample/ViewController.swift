5e4679d283a17693127defc7cf49402ccee66f86
Allen Ussher <allen@ussher.ca>
2020-02-09 22:39:57 -0800

    Initial Commit

---
+++ SqliteSwiftExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SqliteSwiftExample
+//
+//  Created by Allen Ussher on 2/9/20.
+//  Copyright © 2020 Ussher Press. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
